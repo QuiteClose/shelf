47cff6c969eebc5c3e3044bd573b77838742973e
QuiteClose <dean@quiteclose.net>
2025-04-29 22:46:25 -0700

    Add timestamp.sh

---
+++ include/timestamp.sh
@@ -0,0 +1,30 @@
+##############################################################
+# timestamp.sh
+# Records lines of text along with a timestamp. Use with a -
+# to add a note to the last timestamp, or with --all to see
+# with notes.
+##############################################################
+
+function ts() {
+	local STAMP="$(date -u '+%Y%m%d-%H%MZ')"
+	local TXT="timestamps.txt"
+	if [ -z "$1" ]; then
+		grep --color '^\d\S*' <(cat $TXT; echo "$STAMP <<< Current time.")
+	elif [[ "$@" = "--help" ]]; then
+		echo "Usage: ts [OPTIONS] [MESSAGE]"
+		echo "Record a message to timestamps.txt along with a timestamp."
+		echo 
+		echo "  -                Add a note to the last timestamp."
+		echo "  --all            Show all timestamps with notes."
+		echo
+	elif [[ "$@" = "--all" ]]; then
+		grep --color '^\S*' <(cat $TXT; echo "$STAMP <<< Current time.")
+	elif [[ "$@" = "-" ]]; then
+		grep '^\d' $TXT|tail -n1|grep --color '^\S*'
+		echo "Enter notes:"|grep --color '.*'
+		sed 's/^/  /' >> $TXT
+		echo "Use \"ts --all\" to see notes."|grep --color '.*'
+	else
+		echo "$STAMP $@"|tee -a $TXT|grep --color '^\S*'
+	fi
+}
