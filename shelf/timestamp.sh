8b05dbbdfd211c6ff0a72b9444802e56b5cc25cd
QuiteClose <dean@quiteclose.net>
2025-05-06 20:36:12 -0700

    Add tsl for localtime.

--- shelf/timestamp.sh
+++ shelf/timestamp.sh
@@ -6,10 +6,14 @@
 ##############################################################
 
 function ts() {
-	local STAMP="$(date -u '+%y%m%d-%H%MZ')"
+	local STAMP="$(date -u '+%y%m%d.%H%MZ')"
 	local TXT="timestamps.txt"
 	if [ -z "$1" ]; then
-		grep --color '^\d\S*' <(cat $TXT; echo "$STAMP <<< Current time.")
+		if [ -e $TXT ]; then
+			grep --color '^\d\S*' <(cat $TXT; echo "$STAMP <<< Current time.")
+		else
+			echo "$STAMP <<< Current time."|grep --color '^\S*' 
+		fi
 	elif [[ "$@" = "--help" ]]; then
 		echo "Usage: ts [OPTIONS] [MESSAGE]"
 		echo "Record a message to timestamps.txt along with a timestamp."
@@ -28,3 +32,12 @@ function ts() {
 		echo "$STAMP $@"|tee -a $TXT|grep --color '^\S*'
 	fi
 }
+
+function tsl() {
+	ts|sed 's/Z/+0000/'|while read line; do
+		local GIVEN_STAMP="$(awk '{print $1}' <<< $line)"
+		local CONVERTED_STAMP="$(date -j -f '%y%m%d.%H%M%z' $GIVEN_STAMP '+%y%m%d.%H%M%z')"
+		sed "s/$GIVEN_STAMP/$CONVERTED_STAMP/" <<< $line
+	done|grep --color '^\S*'
+}
+
