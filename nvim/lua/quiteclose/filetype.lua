057f83e94aae75cf1fa59ea24aae1679d9ec9cda
QuiteClose <dean@quiteclose.net>
2025-07-26 19:22:33 +0000

    Refactor indentation

--- nvim/lua/quiteclose/filetype.lua
+++ nvim/lua/quiteclose/filetype.lua
@@ -1,52 +1,56 @@
+-- Filetype-specific indentation settings
+local SPACES  = 'spaces'
+local PER_TAB = 'per-tab'
+local DEFAULT = {width = 2, fill = SPACES}
 
-local SPACES  = 0
-local PER_TAB = 1
+local INDENTATION_STYLES = {
+  {2, SPACES,  'lua,html,css,scss,javascript'},
+  {4, SPACES,  'markdown,python,txt'},
+  {2, PER_TAB, 'zig'},
+  {4, PER_TAB, 'Dockerfile,shell,go,Makefile'},
+}
 
 function setWhitespace(width, fill)
+  vim.opt.shiftwidth = width
+  vim.opt.tabstop = width
+  if fill == SPACES then
+    vim.opt.expandtab = true
+    vim.opt.listchars = {
+      lead = " ",
+      nbsp = "~",
+      tab = "··",
+      trail = "·",
+    }
+  else
+    vim.opt.expandtab = false
+    vim.opt.listchars = {
+      lead = "·",
+      nbsp = "~",
+      tab = "  ",
+      trail = "·",
+    }
+  end
+end
+
+function deferWhitespace(width, fill)
   function callback()
+    print("Filetype trigger: " .. vim.bo.filetype .. " | Indent: " .. width .. " " .. fill)
     vim.schedule(function()
-      vim.opt.shiftwidth = width
-      vim.opt.tabstop = width
-      if fill == SPACES then
-        vim.opt.expandtab = true
-        vim.opt.listchars = {
-          lead = " ",
-          nbsp = "~",
-          tab = "··",
-          trail = "·",
-        }
-      else
-        vim.opt.expandtab = false
-        vim.opt.listchars = {
-          lead = "·",
-          nbsp = "~",
-          tab = "  ",
-          trail = "·",
-        }
-      end
+      setWhitespace(width, fill)
     end)
   end
   return callback
 end
 
+
+-- Apply indentation settings
+setWhitespace(DEFAULT.width, DEFAULT.fill)
 vim.api.nvim_create_augroup("setFiletypeWhitespace", { clear = true })
-vim.api.nvim_create_autocmd("Filetype", {
-  group = "setFiletypeWhitespace",
-  pattern = "Dockerfile,shell,go,Makefile",
-  callback = setWhitespace(4, PER_TAB)
-})
-vim.api.nvim_create_autocmd("Filetype", {
-  group = "setFiletypeWhitespace",
-  pattern = "zig",
-  callback = setWhitespace(2, PER_TAB)
-})
-vim.api.nvim_create_autocmd("Filetype", {
-  group = "setFiletypeWhitespace",
-  pattern = "markdown,python,txt",
-  callback = setWhitespace(4, SPACES)
-})
-vim.api.nvim_create_autocmd("Filetype", {
-  group = "setFiletypeWhitespace",
-  pattern = "lua,html,css,scss,javascript",
-  callback = setWhitespace(2, SPACES)
-})
+for _, style in pairs(INDENTATION_STYLES) do
+  local width, fill, pattern = style[1], style[2], style[3]
+  vim.api.nvim_create_autocmd("Filetype", {
+    group = "setFiletypeWhitespace",
+    pattern = pattern,
+    callback = deferWhitespace(width, fill)
+  })
+end
